a8b0e684cc587899d5dc32521d5271a8b701bde3
Kushal Mishra <kuskalm@gmail.com>
2022-04-17 21:39:36 +0530

    first commit

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+// $(document).ready(function () {
+// 	$(window).scroll(function () {
+// 		var scroll = $(window).scrollTop();
+// 		var $nav = $(".nav");
+// 		if (scroll > $nav.height()) {
+// 			$(".nav").css("background", "#dec0de");
+// 			$(".nav").css("transition", "all 500ms");
+// 		} else {
+// 			$(".nav").css("background", "rgba(100,110,110,0)");
+// 		}
+// 	});
+// });
